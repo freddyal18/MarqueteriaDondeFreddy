364717688f195b3a431291cd1f5e27d2331ce4af
freddyal18 <freddya.jaimeso@utadeo.edu.co>
2019-10-31 10:53:04 -0500

    Commit y push de prueba. Vistas VACIAS.

---
+++ app/src/main/java/com/example/marqueteriadondefreddy/EliminarServicio.java
@@ -0,0 +1,14 @@
+package com.example.marqueteriadondefreddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EliminarServicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eliminar_servicio);
+    }
+}
